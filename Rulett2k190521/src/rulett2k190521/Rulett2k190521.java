00067353bcb405fdcb5efc25914ca582b0cc7d03
Prynerd <atti13@gmail.hu>
2019-05-21 13:27:07 +0200

    Starting project

---
+++ Rulett2k190521/src/rulett2k190521/Rulett2k190521.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rulett2k190521;
+
+/**
+ *
+ * @author progmatic
+ */
+public class Rulett2k190521 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
